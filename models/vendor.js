97c28912e95cb1826a7f166a71430029e444ba17
Neeraj Pant <105204939+Neeraj-Pant01@users.noreply.github.com>
2023-04-27 19:03:35 +0530

    final commit

--- models/vendor.js
+++ models/vendor.js
@@ -5,7 +5,7 @@ const vendorSchema = new mongoose.Schema(
       username: { type: String, required: true, unique: true },
       email: { type: String, required: true, unique: true },
       password: { type: String, required: true },
-      isUser: {
+      isStaff:{
         type: Boolean,
         default: true,
       },
